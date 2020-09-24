4226c05e46bb6d73146b2d0d3317c9e1c2ddfc35
jennifer-magpantay <jennifer.magpantay@gmail.com>
2020-09-24 12:42:40 +0100

    Init

---
+++ appointment-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/style/reset.css';
+import '../src/style/index.css';
+import App from '../src/components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
